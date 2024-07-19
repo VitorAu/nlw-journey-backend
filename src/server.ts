c77ec080b7b39f1ed5c4fb70552e93368168313e
VitorAu <vitoraugustosantiago@outlook.com.br>
2024-07-19 20:47:44 -0300

    Aula 01 | Trilha Node.js NLW Journey

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import fastify from "fastify";
+import { prisma } from "./lib/prisma";
+import { createTrip } from "./routes/create-trip";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
+import cors from '@fastify/cors'
+
+const app = fastify()
+
+app.register(cors, {
+    origin: '*',
+    
+})
+
+app.setValidatorCompiler(validatorCompiler);
+app.setValidatorCompiler(serializerCompiler);
+
+app.register(createTrip)
+app.register(confirmTrip)
+
+app.listen({ port: 3333 }).then(() => {
+    console.log('Server running')
+})
